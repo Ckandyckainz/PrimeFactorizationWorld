292ba36a8bcb31b93b104f93d36a06a3e29818b8
Ckandyckainz <74159422+Ckandyckainz@users.noreply.github.com>
2025-04-30 21:23:57 -0700

    everything so far

---
+++ code.js
@@ -0,0 +1,660 @@
+let mcan = document.getElementById("mcan");
+let inventoryGUI = document.getElementById("inventory");
+let toolsGUI = document.getElementById("tools");
+let mcw = window.innerWidth;
+let mch = window.innerHeight;
+mcan.width = mcw;
+mcan.height = mch;
+mcan.style.width = "100%";
+mcan.style.height = "100%";
+let mctx = mcan.getContext("2d");
+let toolHandleColor = colorString(Math.random() * 0.3 + 0.2, Math.random() * 0.3 + 0.2, Math.random() * 0.3 + 0.2, 1);
+let jumping = false;
+let inventory = [];
+let selectedBlock;
+let lastSelectedBlockGUI;
+let breaking = 1;
+
+function drawPick(pick, ctx, x, y) {
+  ctx.strokeStyle = toolHandleColor;
+  ctx.lineWidth = 4;
+  ctx.beginPath();
+  ctx.moveTo(x - 4, y - 4);
+  ctx.lineTo(x + 8, y + 8);
+  ctx.stroke();
+  ctx.strokeStyle = pick.c;
+  ctx.beginPath();
+  ctx.arc(x, y, 8, Math.PI * 7 / 8, Math.PI * 13 / 8);
+  ctx.stroke();
+}
+
+function drawSword(sword, ctx, x, y) {
+  ctx.lineWidth = 4;
+  ctx.strokeStyle = sword.c;
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x - 12, y - 12);
+  ctx.stroke();
+  ctx.strokeStyle = toolHandleColor;
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x + 8, y + 8);
+  ctx.moveTo(x - 4, y + 4);
+  ctx.lineTo(x + 4, y - 4);
+  ctx.stroke();
+}
+
+let drawToolFunctions = [drawPick, drawSword];
+
+class Tool {
+  constructor(type, n) {
+  	this.type = type;
+    this.n = n;
+    this.n1 = -1;
+    this.c = "#FFFFFFFF";
+    this.can = document.createElement("canvas");
+    this.can.width = 32;
+    this.can.height = 32;
+    toolsGUI.appendChild(this.can);
+    this.ctx = this.can.getContext("2d");
+    this.drawSelf = drawToolFunctions[type];
+    this.drawSelf(this, this.ctx, 16, 16);
+    this.ctx.font = "16px serif";
+    this.ctx.fillStyle = "#000000FF";
+    this.ctx.fillText("".concat(this.n), 0, 32);
+    this.can.addEventListener("click", ()=>{
+      let sbi = -3;
+      let ti = -1;
+      for (let i=0; i<primes.length; i++) {
+        if (primes[i].n == selectedBlock) {
+          sbi = i;
+        }
+        if (primes[i].n == this.n) {
+          ti = i;
+        }
+      }
+      if (sbi == ti+1) {
+        let index = 0;
+        for (let i=0; i<inventory.length; i++) {
+          if (inventory[i].n == selectedBlock) {
+            index = i;
+          }
+        }
+        if (inventory[index].a >= selectedBlock**2) {
+          this.n = selectedBlock;
+          this.n1 = primes[sbi+1].n;
+          this.c = colorString(...primes[sbi].c, 1);
+          this.drawSelf(this, this.ctx, 16, 16);
+          inventory[index].a -= selectedBlock**2;
+        }
+      }
+    });
+  }
+}
+
+let tools = [];
+tools.push(new Tool(0, 1));
+tools.push(new Tool(1, 1));
+let pick = tools[0];
+
+class Block {
+  constructor(n) {
+    this.n = n;
+    if (n != 1) {
+      this.c = [1, 1, 1, 1];
+      this.noise = 1;
+      for (let i = 0; i < pfs[n].length; i++) {
+        this.c[0] *= primes[pfs[n][i]].c[0] * 0.8;
+        this.c[1] *= primes[pfs[n][i]].c[1] * 0.8;
+        this.c[2] *= primes[pfs[n][i]].c[2] * 0.8;
+        this.noise *= primes[pfs[n][i]].noise;
+      }
+      this.imgdt = new ImageData(16, 16);
+      for (let i = 0; i < 1024; i++) {
+        this.imgdt.data[i] = this.c[i % 4] * 255 * (1 - Math.random() * (1 - this.noise));
+      }
+    } else {
+      this.c = [0, 0, 0, 1];
+      this.imgdt = new ImageData(16, 16);
+    }
+  }
+}
+
+class Chunk {
+  constructor(id) {
+    this.id = id;
+    this.map = [];
+    for (let i = 0; i < 256; i++) {
+      this.map.push({
+        n: 0,
+        b: 0
+      });
+    }
+    this.imgdt = new ImageData(256, 256);
+  }
+  resetImgdt() {
+    for (let i = 0; i < this.map.length; i++) {
+      let b = 0;
+      for (let j = 0; j < 64; j++) {
+        if (setBlock((this.id % 16) * 16 + i % 16 + randomBetween(-12, 13), Math.floor(this.id / 16) * 16 + Math.floor(i / 16) + randomBetween(-12, 13), 0) == 1) {
+          b += 3 / 64;
+        }
+      }
+      if (b > 1) {
+        b = 1;
+      }
+      this.map[i].b = b;
+    }
+    for (let i = 0; i < 262144; i++) {
+      let bx = Math.floor(i / 64) % 16;
+      let by = Math.floor(i / 16384);
+      let px = Math.floor(i / 4) % 16;
+      let py = Math.floor(i / 1024) % 16;
+      if (blocks[this.map[by * 16 + bx].n].n == 1) {
+        if (i % 4 == 3) {
+          this.imgdt.data[i] = 255;
+        } else {
+          this.imgdt.data[i] = 0;
+        }
+      } else {
+        if (i % 4 == 3) {
+          this.imgdt.data[i] = 255;
+        } else {
+          this.imgdt.data[i] = blocks[this.map[by * 16 + bx].n].imgdt.data[py * 64 + px * 4 + i % 4] * this.map[by * 16 + bx].b + (1 - this.map[by * 16 + bx].b) * 255 * uc[i % 4];
+        }
+      }
+    }
+  }
+  resetNearbyImgdts(){
+  	for (let i=-1; i<2; i++) {
+    	for (let j=-1; j<2; j++) {
+      	let nid = this.id+i*256+j;
+      	if (nid >= 0 && nid < 512) {
+          map[nid].resetImgdt();
+        }
+      }
+    }
+  }
+}
+
+function randomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function colorString(r, g, b, a) {
+  let color = Math.floor(r * 255) * 256 ** 3 + Math.floor(g * 255) * 256 ** 2 + Math.floor(b * 255) * 256 + Math.floor(a * 255);
+  return "#" + color.toString(16).padStart(8, "0");
+}
+
+function weightedArrayPick(array, weight) {
+  let choosing = true;
+  let counter = -1;
+  while (choosing) {
+    counter++;
+    if (Math.random() < weight) {
+      choosing = false;
+    }
+  }
+  return array[counter % array.length];
+}
+
+function setBlock(x, y, n) {
+  let inMap = false;
+  if (x >= 0) {
+    if (y >= 0) {
+      if (x <= 255) {
+        if (y <= 511) {
+          inMap = true
+        }
+      }
+    }
+  }
+  if (inMap) {
+    let x2 = Math.floor(x / 16);
+    let y2 = Math.floor(y / 16);
+    if (n == 0) {
+      return map[y2 * 16 + x2].map[(y % 16) * 16 + x % 16].n;
+    } else {
+      if (n == "b") {
+        return map[y2 * 16 + x2].map[(y % 16) * 16 + x % 16].b;
+      } else {
+        map[y2 * 16 + x2].map[(y % 16) * 16 + x % 16].n = n;
+        if (mapGenerated) {
+          map[y2 * 16 + x2].resetNearbyImgdts();
+        }
+      }
+    }
+  } else {
+    return 1;
+  }
+}
+
+function breakBlock(x, y) {
+  let block = setBlock(x, y, 0);
+  let index = 0;
+  while (inventory[index].n != block) {
+    index++;
+    if (inventory[index] == undefined) {
+      seenNewBlock(block);
+      index = 0;
+    }
+  }
+  inventory[index].a++;
+  inventoryGUI.childNodes[index].childNodes[2].innerText = "".concat(inventory[index].a);
+  setBlock(x, y, 1);
+}
+
+function placeBlock(x, y) {
+  if (selectedBlock != undefined) {
+    let index = 0;
+    while (inventory[index].n != selectedBlock) {
+      index++;
+    }
+    if (inventory[index].a > 0) {
+      inventory[index].a--;
+      lastSelectedBlockGUI.childNodes[2].innerText = "".concat(inventory[index].a);
+      setBlock(x, y, selectedBlock);
+    }
+  }
+}
+
+let nMax = randomBetween(1001, 1502);
+let primes = [];
+let pfs = [];
+let blocks = [];
+for (let i = 0; i < nMax; i++) {
+  pfs.push([]);
+}
+for (let i = 2; i < nMax; i++) {
+  if (pfs[i].length == 0) {
+    primes.push({
+      n: i,
+      c: [Math.random() * 0.3 + 0.7, Math.random() * 0.3 + 0.7, Math.random() * 0.3 + 0.7],
+      noise: Math.random() * 0.3 + 0.7
+    });
+    let j = 1;
+    while (i ** j < nMax) {
+      let k = i ** j;
+      while (k < pfs.length) {
+        pfs[k].push(primes.length - 1);
+        k += i ** j;
+      }
+      j++;
+    }
+  }
+  blocks[i] = new Block(i);
+}
+blocks[1] = new Block(1);
+
+let mapGenerated = false;
+let map = [];
+for (let i = 0; i < 512; i++) {
+  let chunk = new Chunk(i);
+  map.push(chunk);
+}
+for (let i = 0; i < 256; i++) {
+  setBlock(i, 511, nMax - 1);
+}
+for (let i = 510; i > -1; i--) {
+  for (let j = 0; j < 256; j++) {
+    let choosing = true;
+    let x = randomBetween(j - 1, j + 2);
+    let y = randomBetween(i + 1, i + 3);
+    while (choosing) {
+      x = randomBetween(j - 1, j + 2);
+      y = randomBetween(i + 1, i + 3);
+      if (x > -1) {
+        if (y > -1) {
+          if (x < 256) {
+            if (y < 512) {
+              choosing = false;
+            }
+          }
+        }
+      }
+    }
+    let set = setBlock(x, y, 0);
+    if (set > 1) {
+      if (Math.random() > 0.05) {
+        setBlock(j, i, set);
+      } else {
+        let factor = 0;
+        pfs[set].forEach((item) => {
+          if (Math.random() < 1 / primes[item].n) {
+            factor = primes[item].n;
+          }
+        });
+        if (factor == 0) {
+          let decrease = true;
+          for (let i = 0; i < pfs[set].length; i++) {
+            if (Math.random() < 0.7) {
+              decrease = false;
+            }
+          }
+          if (decrease) {
+            setBlock(j, i, set - 1);
+          } else {
+            setBlock(j, i, set);
+          }
+        } else {
+          setBlock(j, i, set / factor);
+        }
+      }
+    } else {
+      setBlock(j, i, set);
+    }
+  }
+}
+mapGenerated = true;
+
+function seenNewBlock(block) {
+  let div = document.createElement("div");
+  let next = -1;
+  let nextFound = false;
+  while (!nextFound) {
+    next++;
+    if (inventory[next] == undefined) {
+      inventoryGUI.appendChild(div);
+      inventory.push({
+        n: block,
+        a: 0
+      });
+      nextFound = true;
+    } else {
+      if (inventory[next].n > block) {
+        inventoryGUI.insertBefore(div, inventoryGUI.childNodes[next]);
+        inventory.splice(next, 0, {
+          n: block,
+          a: 0
+        });
+        nextFound = true;
+      }
+    }
+  }
+  let h3n = document.createElement("h3");
+  h3n.innerText = "".concat(block);
+  h3n.style.display = "inline";
+  div.appendChild(h3n);
+  let canvas = document.createElement("canvas");
+  canvas.width = 16;
+  canvas.height = 16;
+  canvas.style.width = "16px";
+  canvas.style.height = "16px";
+  canvas.style.display = "inline";
+  let ctx = canvas.getContext("2d");
+  ctx.putImageData(blocks[block].imgdt, 0, 0);
+  div.appendChild(canvas);
+  let h3a = document.createElement("h3");
+  h3a.innerText = "0";
+  h3a.style.display = "inline";
+  div.appendChild(h3a);
+  div.addEventListener("click", function() {
+    selectedBlock = block;
+    if (lastSelectedBlockGUI != undefined) {
+      lastSelectedBlockGUI.style.border = "none";
+    }
+    div.style.border = "4px solid #FFFFFF";
+    lastSelectedBlockGUI = div;
+  });
+}
+
+let uc = [Math.random() * 0.2 + 0.3, Math.random() * 0.2 + 0.3, Math.random() * 0.2 + 0.3];
+map.forEach((chunk) => {
+  chunk.resetImgdt();
+});
+
+let view = {
+  x: 2000,
+  y: -32,
+  velX: 0,
+  velY: 0,
+  onGround: true
+};
+let keysDown = [];
+
+function keypress(event) {
+  let hasKey = false;
+  keysDown.forEach((key) => {
+    if (key == event.key) {
+      hasKey = true;
+    }
+  });
+  if (!hasKey) {
+    keysDown.push(event.key);
+  }
+}
+document.addEventListener("keydown", keypress);
+
+function keyup(event) {
+  for (let i = 0; i < keysDown.length; i++) {
+    if (keysDown[i] == event.key) {
+      keysDown.splice(i, 1);
+    }
+  }
+}
+document.addEventListener("keyup", keyup);
+
+let mouseTouching = 1;
+let mousePos = {
+  x: 0,
+  y: 0
+}
+
+function mousemove(event) {
+  let x = Math.floor((view.x - mcw / 2 + event.x) / 16);
+  let y = Math.floor((view.y - mch / 2 + event.y) / 16);
+  mouseTouching = 1;
+  if (setBlock(x, y, 0) != 1) {
+    if (setBlock(x, y, "b") > 0.5) {
+      mouseTouching = setBlock(x, y, 0);
+      let seenNew = true;
+      inventory.forEach((item) => {
+        if (mouseTouching == item.n) {
+          seenNew = false;
+        }
+      });
+      if (seenNew) {
+        seenNewBlock(mouseTouching);
+      }
+    }
+  }
+  mousePos.x = event.x;
+  mousePos.y = event.y;
+}
+mcan.addEventListener("mousemove", mousemove);
+
+let mouseDown = false;
+let mouseWasDown = false;
+
+function mousedown() {
+  mouseDown = true;
+}
+mcan.addEventListener("mousedown", mousedown);
+
+function mouseup() {
+  mouseDown = false;
+}
+mcan.addEventListener("mouseup", mouseup);
+
+let cc = {
+  body: [],
+  energy: []
+};
+for (let i = 0; i < 3; i++) {
+  cc.body.push(Math.random() * 0.2 + 0.4);
+}
+for (let i = 0; i < 3; i++) {
+  cc.energy.push(Math.random() * 0.3 + 0.7);
+}
+
+let bounds = {
+  left: false,
+  right: false,
+  up: false,
+  down: false
+};
+let boundChecks = 16;
+
+function determineBounds() {
+  bounds = {
+    left: false,
+    right: false,
+    up: false,
+    down: false
+  };
+  for (let i = -1; i < 2; i++) {
+    if (setBlock(Math.floor(view.x / 16 + i * 0.9), Math.floor(view.y / 16 + 1), 0) != 1) {
+      bounds.down = true;
+    }
+    if (setBlock(Math.floor(view.x / 16 + i * 0.9), Math.floor(view.y / 16 - 1), 0) != 1) {
+      bounds.up = true;
+    }
+    if (setBlock(Math.floor(view.x / 16 - 1), Math.floor(view.y / 16 + i * 0.9), 0) != 1) {
+      bounds.left = true;
+    }
+    if (setBlock(Math.floor(view.x / 16 + 1), Math.floor(view.y / 16 + i * 0.9), 0) != 1) {
+      bounds.right = true;
+    }
+  }
+  if (bounds.down) {
+    if (view.velY > 0) {
+      view.y = Math.round(view.y / 16) * 16;
+      view.velY = 0;
+    }
+    if (bounds.up) {
+      if (view.velY < 0) {
+        view.y -= view.velY * 0.9 / boundChecks;
+        view.y = Math.round(view.y / 16) * 16;
+        view.velY = 0;
+      }
+    }
+    if (bounds.left) {
+      if (view.velX < 0) {
+        view.x = Math.round(view.x / 16) * 16;
+        view.velX = 0;
+      }
+    }
+    if (bounds.right) {
+      if (view.velX > 0) {
+        view.x = Math.round(view.x / 16) * 16;
+        view.velX = 0;
+      }
+    }
+  }
+}
+
+function drawingLoop() {
+  mctx.fillStyle = "#000000FF";
+  mctx.fillRect(0, 0, mcw, mch);
+  for (let i = 0; i < 512; i++) {
+    mctx.putImageData(map[i].imgdt, (i % 16) * 256 - view.x + mcw / 2, Math.floor(i / 16) * 256 - view.y + mch / 2);
+  }
+  mctx.fillStyle = colorString(...cc.body, 1);
+  mctx.fillRect(mcw / 2 - 16, mch / 2 - 16, 32, 32);
+  mctx.strokeStyle = colorString(...cc.energy, 1);
+  mctx.lineWidth = 4;
+  mctx.strokeRect(mcw / 2 - 8, mch / 2 - 8, 16, 16);
+  if (mouseTouching != 1) {
+    mctx.font = "16px serif";
+    mctx.fillStyle = "#FFFFFFFF";
+    mctx.fillText(mouseTouching, mousePos.x, mousePos.y);
+  }
+  if (breaking) {
+    pick.drawSelf(pick, mctx, mousePos.x + 8, mousePos.y + 8);
+  }
+  requestAnimationFrame(drawingLoop);
+}
+drawingLoop();
+
+function physicsLoop() {
+  if (mouseDown) {
+    let x = Math.floor((view.x - mcw / 2 + mousePos.x) / 16);
+    let y = Math.floor((view.y - mch / 2 + mousePos.y) / 16);
+    if (!mouseWasDown) {
+      if (setBlock(x, y, 0) == 1) {
+        breaking = false;
+      } else {
+        breaking = true;
+      }
+    }
+    if (x > -1) {
+      if (y > -1) {
+        if (x < 256) {
+          if (y < 512) {
+            if (x > Math.floor(view.x / 16) - 9) {
+              if (y > Math.floor(view.y / 16) - 9) {
+                if (x < Math.floor(view.x / 16) + 8) {
+                  if (y < Math.floor(view.y / 16) + 8) {
+                    let bn = setBlock(x, y, 0);
+                    if (bn == 1) {
+                      if (!breaking) {
+                        let canPlace = false;
+                        for (let i = -1; i < 2; i++) {
+                          for (let j = -1; j < 2; j++) {
+                            if (setBlock(x + i, y + j, 0) != 1) {
+                              canPlace = true;
+                            }
+                          }
+                        }
+                        if (canPlace) {
+                          placeBlock(x, y);
+                        }
+                      }
+                    } else {
+                      if (breaking) {
+                        let canBreak = true;
+                        if (pick.n == 1) {
+                          if (bn != 2) {
+                            canBreak = false;
+                          }
+                        } else {
+                          pfs[bn].forEach((factor) => {
+                            if (primes[factor].n > pick.n) {
+                              canBreak = false;
+                            }
+                          });
+                          if (bn == pick.n1) {
+                            canBreak = true;
+                          }
+                        }
+                        if (canBreak) {
+                          breakBlock(x, y);
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  mouseWasDown = mouseDown;
+  keysDown.forEach((keyDown) => {
+    if (keyDown == " ") {
+      if (bounds.down) {
+        view.velY = -6;
+        jumping = true;
+      }
+    }
+    if (keyDown == "a") {
+      view.velX = -4;
+    }
+    if (keyDown == "d") {
+      view.velX = 4;
+    }
+  });
+  if (!bounds.down) {
+    view.velY += 0.1;
+  }
+  for (let i = 0; i < boundChecks; i++) {
+    determineBounds();
+    view.x += view.velX / boundChecks;
+    view.y += view.velY / boundChecks;
+  }
+  view.velX *= 0.9;
+  requestAnimationFrame(physicsLoop);
+}
+physicsLoop();
